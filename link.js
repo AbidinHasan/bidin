c481e342419cf542d21cf095294d81898441465b
Bidin <muhammadabidinhasan@gmail.com>
2025-10-24 11:26:26 +0700

    Update Tema

---
+++ link.js
@@ -0,0 +1,18 @@
+document.getElementById("Home").addEventListener("click", function () {
+  window.location.href = "index.html";
+});
+document.getElementById("stp").addEventListener("click", function () {
+  window.location.href = "DebitSTP.html";
+});
+document.getElementById("wwtp").addEventListener("click", function () {
+  window.location.href = "DebitWWTP.html";
+});
+document.getElementById("internalLAB").addEventListener("click", function () {
+  window.location.href = "HasilLAB.html";
+});
+document.getElementById("msds").addEventListener("click", function () {
+  window.location.href = "msds.html";
+});
+document.getElementById("wi").addEventListener("click", function () {
+  window.location.href = "wi.html";
+});
